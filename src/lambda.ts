15b3af58c7f0e647b63654524f78351916fa2128
Richard Garbutt <richard@1000mileweb.com>
2021-09-09 19:25:45 +0100

    Initial SST project setup

---
+++ src/lambda.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
+import * as words from "an-array-of-english-words"
+
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2
+) => {
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `The random word is ${randomWord}. Generated at ${event.requestContext.time}`
+  };
+};
